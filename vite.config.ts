75be4ab33cd4eacea9d21297bb5bed8a206d624b
Shanu Jha <shanu.j@foxberry.in>
2024-09-16 23:12:30 +0530

    Addign base url replacement automatic for favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from 'vite-plugin-html';
 
+const base_url = '/starter-shadcn/';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/starter-shadcn/',
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          BASE_URL: base_url, // This should match the base URL
+        },
+      },
+    }),
+  ],
+  base: base_url,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
